270a13766c4596d0bbf1cf0cbda940df0d4096f0
vkyscen <vickyeditz@gmail.com>
2021-05-10 16:30:55 +0530

    Gradient Image BG slider done ->Home Component

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home-container">
+        <img
+          className="home-image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
+
+      <div className="home-row">
+        {/* Product card */}
+        {/* Product card */}
+      </div>
+
+      <div className="home-row">
+        {/* Product card */}
+        {/* Product card */}
+        {/* Product card */}
+      </div>
+
+      <div className="home-row">
+        {/* Product card */}
+        {/* Product card */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
